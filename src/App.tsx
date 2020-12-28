a28641140897938a6470a508427c72433bbae24b
Tatsuro Baba <harakirisoul@gmail.com>
2020-12-28 21:47:32 +0900

    The first commit of docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+type Doc = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 function App() {
+  const docs = [
+    {
+      id: 1,
+      title: '最初のドキュメント',
+      body: 'これが俺達の最初のドキュメントですよっと。\n分かる？',
+    },
+    {
+      id: 2,
+      title: '2つ目のドキュメント',
+      body: '2つ目のドキュメントだよ。ちょっと適当。',
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Kay</h1>
       </header>
+      <div className="docs">
+        {docs.map((document: Doc) => (
+          <article key={document.id}>
+            <header>
+              <h2>{document.title}</h2>
+            </header>
+            <div className="docs-body">{document.body}</div>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
